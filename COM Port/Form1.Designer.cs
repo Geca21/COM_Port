bf67cdc69348eb7ef0ac4ad76abcd880bd266934
Geca21 <metylan@mail.ru>
2021-02-11 19:56:02 +0300

    Добавьте файлы проекта.

---
+++ COM Port/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace COM_Port
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.comPort = new System.Windows.Forms.ComboBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.infoPanel = new System.Windows.Forms.StatusStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtRec = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnRec = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.labelInrfoPanel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.infoPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.12426F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 55.6213F));
+            this.tableLayoutPanel1.Controls.Add(this.btnOpen, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn_Close, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comPort, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.96629F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(821, 64);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // comPort
+            // 
+            this.comPort.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.comPort.FormattingEnabled = true;
+            this.comPort.Location = new System.Drawing.Point(3, 21);
+            this.comPort.Name = "comPort";
+            this.comPort.Size = new System.Drawing.Size(253, 21);
+            this.comPort.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btnOpen.Location = new System.Drawing.Point(276, 20);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Открыть";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.btn_Close.Location = new System.Drawing.Point(367, 20);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(75, 23);
+            this.btn_Close.TabIndex = 2;
+            this.btn_Close.Text = "Закрыть";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // infoPanel
+            // 
+            this.infoPanel.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.labelInrfoPanel});
+            this.infoPanel.Location = new System.Drawing.Point(0, 388);
+            this.infoPanel.Name = "infoPanel";
+            this.infoPanel.Size = new System.Drawing.Size(845, 22);
+            this.infoPanel.TabIndex = 1;
+            this.infoPanel.Text = "statusStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtRec);
+            this.panel1.Location = new System.Drawing.Point(12, 207);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(821, 100);
+            this.panel1.TabIndex = 3;
+            // 
+            // txtRec
+            // 
+            this.txtRec.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtRec.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtRec.Location = new System.Drawing.Point(0, 0);
+            this.txtRec.Multiline = true;
+            this.txtRec.Name = "txtRec";
+            this.txtRec.Size = new System.Drawing.Size(821, 100);
+            this.txtRec.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 89.76858F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.23143F));
+            this.tableLayoutPanel2.Controls.Add(this.btnRec, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 305);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(821, 80);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // btnRec
+            // 
+            this.btnRec.Location = new System.Drawing.Point(739, 3);
+            this.btnRec.Name = "btnRec";
+            this.btnRec.Size = new System.Drawing.Size(75, 23);
+            this.btnRec.TabIndex = 0;
+            this.btnRec.Text = "Получить";
+            this.btnRec.UseVisualStyleBackColor = true;
+            this.btnRec.Click += new System.EventHandler(this.btnRec_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 90.01218F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.98782F));
+            this.tableLayoutPanel3.Controls.Add(this.btnSend, 1, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(12, 169);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(821, 32);
+            this.tableLayoutPanel3.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(741, 3);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Отправить";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtSend);
+            this.panel2.Location = new System.Drawing.Point(12, 70);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(821, 100);
+            this.panel2.TabIndex = 6;
+            // 
+            // txtSend
+            // 
+            this.txtSend.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtSend.Location = new System.Drawing.Point(0, 0);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(821, 100);
+            this.txtSend.TabIndex = 0;
+            // 
+            // labelInrfoPanel
+            // 
+            this.labelInrfoPanel.ForeColor = System.Drawing.Color.Black;
+            this.labelInrfoPanel.Name = "labelInrfoPanel";
+            this.labelInrfoPanel.Overflow = System.Windows.Forms.ToolStripItemOverflow.Always;
+            this.labelInrfoPanel.Size = new System.Drawing.Size(131, 17);
+            this.labelInrfoPanel.Text = "toolStripStatusLabel1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 410);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.infoPanel);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "COM Port";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.infoPanel.ResumeLayout(false);
+            this.infoPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.StatusStrip infoPanel;
+        private System.Windows.Forms.ComboBox comPort;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnRec;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtRec;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ToolStripStatusLabel labelInrfoPanel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ COM Port/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COM_Port
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] port = System.IO.Ports.SerialPort.GetPortNames();
+                comPort.Items.AddRange(port);
+                comPort.SelectedIndex = 0;
+                btn_Close.Enabled = false;
+                labelInrfoPanel.ForeColor = Color.Black;
+                if (serialPort1.IsOpen)
+                {
+                    labelInrfoPanel.Text = "Порт: "+ serialPort1.IsOpen.ToString();
+                }
+                else { labelInrfoPanel.Text = "Порт: " + serialPort1.IsOpen.ToString(); }
+
+                }
+            catch (Exception ex)
+            {
+                labelInrfoPanel.Text = ex.Message;
+                labelInrfoPanel.ForeColor = Color.DarkRed;
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = false;
+            btn_Close.Enabled = true;
+            try
+            {
+                serialPort1.PortName = comPort.Text;
+                serialPort1.Open();
+                labelInrfoPanel.Text = "Порт: " + serialPort1.IsOpen.ToString();
+            }
+            catch(Exception ex)
+            {
+                labelInrfoPanel.Text = ex.Message;
+            }
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = true;
+            btn_Close.Enabled = false;
+            try
+            {
+                serialPort1.PortName = comPort.Text;
+                serialPort1.Close();
+            }
+            catch (Exception ex)
+            {
+                labelInrfoPanel.Text = ex.Message;
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(serialPort1.IsOpen)
+                {
+                    serialPort1.WriteLine(txtSend.Text + Environment.NewLine);
+                    txtSend.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                labelInrfoPanel.Text = ex.Message;
+            }
+        }
+
+        
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                txtRec.Text = serialPort1.ReadExisting();
+            }
+            catch (Exception ex)
+            {
+                labelInrfoPanel.Text = ex.Message;
+            }
+        }
+
+        private void btnRec_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtRec.Text = serialPort1.ReadExisting();
+            }
+            catch (Exception ex)
+            {
+                labelInrfoPanel.Text = ex.Message;
+            }
+        }
+    }
+}
